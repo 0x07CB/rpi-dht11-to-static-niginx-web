f5150639110b5fd06735f4728262cbfc786e92c7
0x07CB <83157348+0x07CB@users.noreply.github.com>
2022-02-26 17:35:27 +0100

    not done but dht11 poo write better and no while

--- exemple-dht11.py
+++ exemple-dht11.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import sys
 import Adafruit_DHT
-
-while True:
+def get_info():
     humidity, temperature = Adafruit_DHT.read_retry(11, 4) #11 for dht11, 4 is the gpio i think
+def send_to_out_formated():
     print ("Temp: {0:0.1f} C  Humidity: {1:0.1f} %").format(
         temperature,
         humidity
