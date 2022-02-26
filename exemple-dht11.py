a4b24d00455bf8b235eaa504f9aa760bad0f7a4c
0x07CB <0x07CB@users.noreply.github.com>
2022-02-26 17:46:51 +0100

    poo begin
    rewrite in progress let's me an moment

--- exemple-dht11.py
+++ exemple-dht11.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 import sys
 import Adafruit_DHT
-def get_info():
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4) #11 for dht11, 4 is the gpio i think
-def send_to_out_formated():
-    print ("Temp: {0:0.1f} C  Humidity: {1:0.1f} %").format(
-        temperature,
-        humidity
-        )
-    
+class dht(Object):
+    def __init__(self,gpio_dht,dht_type):
+        self.dht,self.gpio = gpio_dht,dht_type
+        
+    def get_info(self):
+        humidity, temperature = Adafruit_DHT.read_retry(11, 4) #11 for dht11, 4 is the gpio i think
+        
+    def send_to_out_formated(self):
+        print ("Temp: {0:0.1f} C  Humidity: {1:0.1f} %").format(
+            temperature,
+            humidity
+            )
+            
+    def send_to_out_raw(self,temp=True,humd=True,conv_space=1):
+        pass
+        
