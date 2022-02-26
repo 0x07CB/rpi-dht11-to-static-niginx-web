c692092bea4ce1782bb1e68e9cff8c8d2f5db83c
0x07CB <83157348+0x07CB@users.noreply.github.com>
2022-02-26 17:31:58 +0100

    rewrite in python3 and comment

--- exemple-dht11.py
+++ exemple-dht11.py
@@ -1,9 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import sys
 import Adafruit_DHT
 
 while True:
-
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-
-    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4) #11 for dht11, 4 is the gpio i think
+    print ("Temp: {0:0.1f} C  Humidity: {1:0.1f} %").format(
+        temperature,
+        humidity
+        )
+    
