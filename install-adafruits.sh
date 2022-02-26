8e2655cd2088fcd1557ab81dad76f43fc0601a68
0x07CB <0x07CB@users.noreply.github.com>
2022-02-27 00:51:47 +0100

    modifications vers une installation generaliste
    i think to replace a lot of things into an system for use the project
    because, that have need a lot of things and I'm sure it's better if I
    start to take in charge: arch and debian ...
    
    but I want do more: an esasy to deploy web meteo station with an nginx
    auto configured.
    
    and other, I go base this on automatic scripts.
    first.

--- install-adafruits.sh
+++ install-adafruits.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
+install_bases_requirements(){
+    baseos=$(grep '^ID_LIKE' /etc/os-release)
+    if [ baseos -eq "debian" ]
+    then
+        echo "On met a jour ce system et ensuite..."
+        sudo apt update -y && sudo apt full-upgrade -y && sudo apt install autoremove -y && sudo apt install autoclean -y
+        echo "installation de git-core..."
+        sudo apt-get install -y git-core
+        git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+        sudo apt-get install build-essential python-dev
+        cd Adafruit_Python_DHT
+        sudo python setup.py install
+    else
+    
+    
+    fi
+    
+}
 
-sudo apt-get install git-core
-git clone https://github.com/adafruit/Adafruit_Python_DHT.git
-sudo apt-get install build-essential python-dev
-cd Adafruit_Python_DHT
-sudo python setup.py install
+
+
 
 #need changes for be ok for every linux system's 
