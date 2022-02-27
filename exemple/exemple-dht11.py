4b7e506a61e233481fc6d246842b848b6079fd2f
0x07CB <0x07CB@users.noreply.github.com>
2022-02-27 15:47:06 +0100

    bug fixing patch

--- exemple/exemple-dht11.py
+++ exemple/exemple-dht11.py
@@ -5,10 +5,10 @@ import json
 #
 class dht(object):
     def __init__(self,gpio_dht,dht_type):
-        self.dht,self.gpio = gpio_dht,dht_type
+        self.gpio,self.dht = gpio_dht,dht_type
         
     def get_info(self):
-        self.humidity, self.temperature = Adafruit_DHT.read_retry(self.dht, self.gpio) #11 for dht11, 4 is the gpio i think
+        self.humidity, self.temperature = Adafruit_DHT.read_retry(int(self.dht), int(self.gpio)) #11 for dht11, 4 is the gpio i think
         
     def send_to_out_formated(self):
         print ("Temp: {0:0.1f} C  Humidity: {1:0.1f} %").format(
@@ -17,8 +17,8 @@ class dht(object):
             )
             
     def send_to_out(self,temp=True,humd=True,conv_space=1):
-        t = "{0:0.1f}".format(self.temperature)
-        h = "{0:0.1f}".format(self.humidity)
+        t = "{}".format(self.temperature)
+        h = "{}".format(self.humidity)
         space = " " * conv_space
         if not temp: 
             t=""
@@ -26,13 +26,13 @@ class dht(object):
         if not humd: 
             h=""
             space=""
-        print ("{t}{space}{h}").format(
+        print ("{t}{space}{h}".format(
             t=t,
             space=space,
             h=h
-            )
+            ))
 #       
-config = json.loads(str(sys.stdin))
+config = json.load(sys.stdin)
 DHTsensor=dht(config["gpio"],config["dht"])
 DHTsensor.get_info()
 if config["display"]["temperature"] or config["display"]["humidity"]:
