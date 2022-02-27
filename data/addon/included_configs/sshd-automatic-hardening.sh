a0bcb90b3cb5195adbc5b82d3f7c8897c5f7ac86
0x07CB <0x07CB@users.noreply.github.com>
2022-02-27 17:40:24 +0100

    robot robot
    bip bop bip

--- data/addon/included_configs/sshd-automatic-hardening.sh
+++ data/addon/included_configs/sshd-automatic-hardening.sh
@@ -8,21 +8,32 @@ function made_new_sshd(){
     echo "Protocol 2" > /tmp/sshd/sshd_config
     echo "Port $port" > /tmp/sshd/sshd_config
     echo "LogLevel VERBOSE" > /tmp/sshd/sshd_config
-    echo "# Authentication:" > /tmp/sshd/sshd_config
     echo "PermitRootLogin no" > /tmp/sshd/sshd_config
     echo "MaxAuthTries 3" > /tmp/sshd/sshd_config
     echo "MaxSessions 5" > /tmp/sshd/sshd_config
+    echo "PubkeyAuthentication yes" > /tmp/sshd/sshd_config
+    echo "AuthorizedKeysFile	.ssh/authorized_keys" > /tmp/sshd/sshd_config
     echo "HostbasedAuthentication no" > /tmp/sshd/sshd_config
     echo "PermitEmptyPasswords no" > /tmp/sshd/sshd_config
     echo "ChallengeResponseAuthentication yes" > /tmp/sshd/sshd_config
     echo "UsePAM yes" > /tmp/sshd/sshd_config
     echo "X11Forwarding no" > /tmp/sshd/sshd_config
     echo "PrintMotd no" > /tmp/sshd/sshd_config
-    echo "ClientAliveInterval 600" > /tmp/sshd/sshd_config
+    echo "ClientAliveInterval 300" > /tmp/sshd/sshd_config
     echo "ClientAliveCountMax 0" > /tmp/sshd/sshd_config
     echo "AllowUsers $admin $user $monitor" > /tmp/sshd/sshd_config
     echo "AuthenticationMethods publickey,keyboard-interactive" > /tmp/sshd/sshd_config
     echo "PasswordAuthentication no" > /tmp/sshd/sshd_config
+    echo "PermitEmptyPasswords no" > /tmp/sshd/sshd_config
+    echo "KbdInteractiveAuthentication yes" > /tmp/sshd/sshd_config
+    echo "Compression $compress" > /tmp/sshd/sshd_config
+    if [ $sftpsub -eq "yes" ]
+        then
+        echo "Subsystem  sftp	/usr/lib/ssh/sftp-server" > /tmp/sshd/sshd_config
+    else
+        echo "#Warning#Subsystem  sftp	/usr/lib/ssh/sftp-server#Warning#" > /tmp/sshd/sshd_config
+    fi
+    #echo "#Banner none"
     cp /tmp/sshd/sshd_config /etc/ssh/sshd_config
 }
 
@@ -31,7 +42,7 @@ baseos=$(grep '^ID_LIKE' /etc/os-release)
 if [ baseos -eq "debian" ]
     then
     sudo apt update -y && sudo apt install -y fail2ban 
-    sudo cp /etc/ssh/sshd_config etc/ssh/sshd_config- && echo "backup of sshd configuration ok."
+    sudo cp /etc/ssh/sshd_config etc/ssh/sshd_config-old && echo "backup of sshd configuration ok."
     echo ""
     echo "	$OpenBSD: sshd_config,v 1.104 2021/07/02 05:11:21 dtucker Exp $"
     echo "This is the sshd server system-wide configuration file.  See sshd_config(5) for more information. "
