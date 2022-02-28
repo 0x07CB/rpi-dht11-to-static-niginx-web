77d7ba1bcc7ea03a0681068a171050b9f38f818d
0x07CB <0x07CB@users.noreply.github.com>
2022-02-28 01:42:28 +0100

    update

--- data/addon/included_configs/endlessh-auto-deploy.sh
+++ data/addon/included_configs/endlessh-auto-deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+function getspe(){ # "special-message" 
+    cd /opt/
+    git clone https://github.com/skeeto/endlessh.git
+    cd endlessh
+    git checkout special-message && git fetch && gut pull
+}
 
-git clone https://github.com/skeeto/endlessh.git
-git checkout special-message && git fetch && gut pull
+function makeinstall(){
+    
+
+}
