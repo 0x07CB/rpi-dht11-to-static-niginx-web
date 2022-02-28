01b562d153189327b7d6463a8416ad9d7a21b45b
0x07CB <0x07CB@users.noreply.github.com>
2022-02-28 12:42:42 +0100

    now i think that take matter
    great but untested

---
+++ data/because/deploy-user-ohmyzsh.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ~/
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh 
+cp ~/.zshrc ~/.zshrc.orig
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+chsh -s $(which zsh)
+#and here the configuration later for cange the .zshrc with good things.
+zshrc_conf="the hacker tricks become"
