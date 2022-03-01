2020db40da86219777ec9f5003f8fe4e362fd3d6
0x07CB <0x07CB@users.noreply.github.com>
2022-03-01 19:23:38 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,8 @@
-FROM debian:latest 
-RUN apt update -y && apt install sudo -y
-RUN sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
-RUN mkdir -p /deploy
-WORKDIR /deploy
-COPY data/ /root/data/
-RUN ls /root/
-RUN sudo bash /root/data/install.sh
+FROM arch:latest 
+RUN pacman -Syy && pacman -Syu
+RUN mkdir -p ~/deploy
+WORKDIR ~/deploy
+COPY ./data/ ~/data/pm
+RUN bash ~/data/install.sh
 CMD /bin/bash
 
--- data/sub/endlessh-auto-deploy.sh
+++ data/sub/endlessh-auto-deploy.sh
@@ -4,21 +4,13 @@ function getspe(){ # "special-message"
     git clone https://github.com/skeeto/endlessh.git
     cd endlessh
     git checkout special-message && git fetch && gut pull
-}
-
-function makeinstall(){
     #use parts
     sudo make
     sudo chmod +x endlessh
     sudo chmod 600 endlessh
     sudo cp endlessh /usr/local/bin/endlessh
-}
-
-function builddocker(){
     sudo docker build .
 }
 
-getspe()
-makeinstall()
-builddocker()
+getspe
 #need an function to deploy one
