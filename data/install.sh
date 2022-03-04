8af83df29b115c6a3b70a44b485b85b987360af4
0x07CB <83157348+0x07CB@users.noreply.github.com>
2022-03-04 02:04:03 +0100

    fix  error

--- data/install.sh
+++ data/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p /opt/dht/
-bash sub/install-and-hardening1.sh
-bash sub/because/deploy-user-ohmyzsh.sh
-cp sub/addon/included_programs/config.json /opt/dht/config.json
+bash data/sub/install-and-hardening1.sh
+bash data/sub/because/deploy-user-ohmyzsh.sh
+cp data/sub/addon/included_programs/config.json /opt/dht/config.json
