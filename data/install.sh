3ad405d1bd2dd11829e2e6cc16fcc3cbfc55fb05
0x07CB <83157348+0x07CB@users.noreply.github.com>
2022-03-04 02:10:36 +0100

    firx error

--- data/install.sh
+++ data/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p /opt/dht/
-bash data/sub/install-and-hardening1.sh
-bash data/sub/because/deploy-user-ohmyzsh.sh
-cp data/sub/addon/included_programs/config.json /opt/dht/config.json
+bash /root/data/sub/install-and-hardening1.sh
+bash /root/data/sub/because/deploy-user-ohmyzsh.sh
+cp /root/data/sub/addon/included_programs/config.json /opt/dht/config.json
